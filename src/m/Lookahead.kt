fd500fa76fe8660585c04d838838c9ba9bd80705
aedans <aedan.smith@gmail.com>
2017-07-24 20:14:36 -0700

    More misc refactoring

--- src/m/Lookahead.kt
+++ src/m/Lookahead.kt
@@ -4,20 +4,14 @@ package m
  * Created by Aedan Smith.
  */
 
-interface LookaheadIterator<out T> : Iterator<T> {
-    operator fun get(i: Int): T
-    fun drop(i: Int)
-    override fun hasNext(): Boolean
-}
-
-class IteratorLookaheadIterator<out T>(private val iterator: Iterator<T>) : LookaheadIterator<T> {
+class LookaheadIterator<out T>(private val iterator: Iterator<T>) : Iterator<T> {
     private var cache = ArrayList<T>()
     private fun fillCache(i: Int) {
         while (cache.size <= i && iterator.hasNext())
             cache.add(iterator.next())
     }
 
-    override fun get(i: Int): T {
+    operator fun get(i: Int): T {
         fillCache(i)
         return cache[i]
     }
@@ -25,7 +19,7 @@ class IteratorLookaheadIterator<out T>(private val iterator: Iterator<T>) : Look
     override fun hasNext() = cache.isNotEmpty() || iterator.hasNext()
     override fun next() = this[0].also { drop(1) }
 
-    override tailrec fun drop(i: Int) {
+    tailrec fun drop(i: Int) {
         if (i != 0) {
             cache.removeAt(0)
             drop(i - 1)
@@ -38,4 +32,4 @@ fun <T, R> LookaheadIterator<T>.collect(collector: (LookaheadIterator<T>) -> R) 
     override fun next(): R = collector(this@collect)
 }
 
-fun <T> Iterator<T>.lookaheadIterator() = IteratorLookaheadIterator(this)
+fun <T> Iterator<T>.lookaheadIterator() = LookaheadIterator(this)
--- src/m/Repl.kt
+++ src/m/Repl.kt
@@ -1,7 +1,9 @@
 package m
 
 import java.io.InputStream
+import java.io.OutputStream
 import java.io.PrintStream
+import kotlin.reflect.KClass
 
 /**
  * Created by Aedan Smith.
@@ -57,4 +59,34 @@ data class RuntimeEnvironment(val symbolTable: SymbolTable, val memory: Memory) 
         symbolTable.setLocation(name, location)
         location.set(memory, obj)
     }
+}
+
+fun getDefaultEnvironment(out: OutputStream): RuntimeEnvironment {
+    val env = RuntimeEnvironment(IRSymbolTable(), Memory(Stack(), Heap()))
+
+    env.setVar("true", true)
+    env.setVar("false", false)
+    env.setVar("nil", Nil)
+
+    env.setVar("cons", mFunction<Any, Any, ConsCell> { car, cdr -> ConsCell(car, cdr) })
+    env.setVar("car", mFunction<ConsCell, Any> { it.car })
+    env.setVar("cdr", mFunction<ConsCell, Any> { it.cdr })
+
+    env.setVar("!", mFunction<Boolean, Boolean> { !it })
+    env.setVar("|", mFunction<Boolean, Boolean, Boolean> { x, y -> x || y })
+    env.setVar("&", mFunction<Boolean, Boolean, Boolean> { x, y -> x && y })
+
+    env.setVar("=", mFunction<Any, Any, Boolean> { x, y -> x == y })
+    env.setVar("<", mFunction<Int, Int, Boolean> { x, y -> x < y })
+    env.setVar(">", mFunction<Int, Int, Boolean> { x, y -> x > y })
+    env.setVar("+", mFunction<Int, Int, Int> { x, y -> x + y })
+    env.setVar("-", mFunction<Int, Int, Int> { x, y -> x - y })
+    env.setVar("*", mFunction<Int, Int, Int> { x, y -> x * y })
+    env.setVar("/", mFunction<Int, Int, Int> { x, y -> x / y })
+
+    env.setVar("print", mFunction<Any, Unit> { PrintStream(out).print(it) })
+    env.setVar("println", mFunction<Any, Unit> { PrintStream(out).println(it) })
+    env.setVar("class-of", mFunction<Any, KClass<*>> { it::class })
+
+    return env
 }
