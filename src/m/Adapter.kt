51abedebfba8adbd1fee4206658c3d11cec4b167
Aedan <aedan.smith@gmail.com>
2017-08-20 22:08:34 -0700

    Change function toString to simpleName

--- src/m/Adapter.kt
+++ src/m/Adapter.kt
@@ -12,14 +12,14 @@ inline fun <reified I, reified O> mFunction(
         crossinline mFunction: (I) -> O
 ) = object : (I) -> O {
     override fun invoke(p1: I) = mFunction(p1)
-    override fun toString() = "(${I::class.qualifiedName}) -> ${O::class.qualifiedName}"
+    override fun toString() = "(${I::class.simpleName}) -> ${O::class.simpleName}"
 }
 
 inline fun <reified I1, reified I2, reified O> mFunction(
         crossinline mFunction: (I1, I2) -> O
 ) = object : (I1) -> (I2) -> O {
     override fun invoke(p1: I1): (I2) -> O = mFunction { i1: I2 -> mFunction(p1, i1) }
-    override fun toString() = "(${I1::class.qualifiedName}) -> (${I2::class.qualifiedName}) -> ${O::class.qualifiedName}"
+    override fun toString() = "(${I1::class.simpleName}) -> (${I2::class.simpleName}) -> ${O::class.simpleName}"
 }
 
 interface ConsList<out T : Any> : Iterable<T> {
