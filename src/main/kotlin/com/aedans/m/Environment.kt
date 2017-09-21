28cb22099acf92163d11302e0e6a3d0a4387a978
Aedan <aedan.smith@gmail.com>
2017-09-20 17:45:47 -0700

    Use apply syntax

--- src/main/kotlin/com/aedans/m/Environment.kt
+++ src/main/kotlin/com/aedans/m/Environment.kt
@@ -22,68 +22,66 @@ fun getDefaultRuntimeEnvironment(
         `in`: InputStream = System.`in`,
         out: OutputStream = System.out,
         err: OutputStream = System.err
-): RuntimeEnvironment {
-    val env = RuntimeEnvironment(IRSymbolTable(), Memory(Heap(0) { Nil }, Stack()))
+) = RuntimeEnvironment(IRSymbolTable(), Memory(Heap(0) { Nil }, Stack())).apply {
+    setVar("true", true)
+    setVar("false", false)
+    setVar("nil", Nil)
 
-    env.setVar("true", true)
-    env.setVar("false", false)
-    env.setVar("nil", Nil)
+    setVar("cons", mFunction<Any, ConsList<Any>, ConsList<Any>>(::ConsCell))
+    setVar("car", mFunction(ConsList<Any>::car))
+    setVar("cdr", mFunction(ConsList<Any>::cdr))
 
-    env.setVar("cons", mFunction<Any, ConsList<Any>, ConsList<Any>>(::ConsCell))
-    env.setVar("car", mFunction(ConsList<Any>::car))
-    env.setVar("cdr", mFunction(ConsList<Any>::cdr))
+    setVar("!", mFunction(Boolean::not))
+    setVar("|", mFunction(Boolean::or))
+    setVar("&", mFunction(Boolean::and))
+    setVar("=", mFunction(Any::equals))
 
-    env.setVar("!", mFunction(Boolean::not))
-    env.setVar("|", mFunction(Boolean::or))
-    env.setVar("&", mFunction(Boolean::and))
-    env.setVar("=", mFunction(Any::equals))
+    setVar("+i", mFunction<Int, Int, Int>(Int::plus))
+    setVar("-i", mFunction<Int, Int, Int>(Int::minus))
+    setVar("*i", mFunction<Int, Int, Int>(Int::times))
+    setVar("/i", mFunction<Int, Int, Int>(Int::div))
+    setVar("%i", mFunction<Int, Int, Int>(Int::rem))
+    setVar("+l", mFunction<Long, Long, Long>(Long::plus))
+    setVar("-l", mFunction<Long, Long, Long>(Long::minus))
+    setVar("*l", mFunction<Long, Long, Long>(Long::times))
+    setVar("/l", mFunction<Long, Long, Long>(Long::div))
+    setVar("%l", mFunction<Long, Long, Long>(Long::rem))
+    setVar("+f", mFunction<Float, Float, Float>(Float::plus))
+    setVar("-f", mFunction<Float, Float, Float>(Float::minus))
+    setVar("*f", mFunction<Float, Float, Float>(Float::times))
+    setVar("/f", mFunction<Float, Float, Float>(Float::div))
+    setVar("%f", mFunction<Float, Float, Float>(Float::rem))
+    setVar("+d", mFunction<Double, Double, Double>(Double::plus))
+    setVar("-d", mFunction<Double, Double, Double>(Double::minus))
+    setVar("*d", mFunction<Double, Double, Double>(Double::times))
+    setVar("/d", mFunction<Double, Double, Double>(Double::div))
+    setVar("%d", mFunction<Double, Double, Double>(Double::rem))
 
-    env.setVar("+i", mFunction<Int, Int, Int>(Int::plus))
-    env.setVar("-i", mFunction<Int, Int, Int>(Int::minus))
-    env.setVar("*i", mFunction<Int, Int, Int>(Int::times))
-    env.setVar("/i", mFunction<Int, Int, Int>(Int::div))
-    env.setVar("%i", mFunction<Int, Int, Int>(Int::rem))
-    env.setVar("+l", mFunction<Long, Long, Long>(Long::plus))
-    env.setVar("-l", mFunction<Long, Long, Long>(Long::minus))
-    env.setVar("*l", mFunction<Long, Long, Long>(Long::times))
-    env.setVar("/l", mFunction<Long, Long, Long>(Long::div))
-    env.setVar("%l", mFunction<Long, Long, Long>(Long::rem))
-    env.setVar("+f", mFunction<Float, Float, Float>(Float::plus))
-    env.setVar("-f", mFunction<Float, Float, Float>(Float::minus))
-    env.setVar("*f", mFunction<Float, Float, Float>(Float::times))
-    env.setVar("/f", mFunction<Float, Float, Float>(Float::div))
-    env.setVar("%f", mFunction<Float, Float, Float>(Float::rem))
-    env.setVar("+d", mFunction<Double, Double, Double>(Double::plus))
-    env.setVar("-d", mFunction<Double, Double, Double>(Double::minus))
-    env.setVar("*d", mFunction<Double, Double, Double>(Double::times))
-    env.setVar("/d", mFunction<Double, Double, Double>(Double::div))
-    env.setVar("%d", mFunction<Double, Double, Double>(Double::rem))
+    setVar("<i", mFunction { x: Int, y: Int -> x < y })
+    setVar(">i", mFunction { x: Int, y: Int -> x > y })
+    setVar("<l", mFunction { x: Long, y: Long -> x < y })
+    setVar(">l", mFunction { x: Long, y: Long -> x > y })
+    setVar("<f", mFunction { x: Float, y: Float -> x < y })
+    setVar(">f", mFunction { x: Float, y: Float -> x > y })
+    setVar("<d", mFunction { x: Double, y: Double -> x < y })
+    setVar(">d", mFunction { x: Double, y: Double -> x > y })
 
-    env.setVar("<i", mFunction { x: Int, y: Int -> x < y })
-    env.setVar(">i", mFunction { x: Int, y: Int -> x > y })
-    env.setVar("<l", mFunction { x: Long, y: Long -> x < y })
-    env.setVar(">l", mFunction { x: Long, y: Long -> x > y })
-    env.setVar("<f", mFunction { x: Float, y: Float -> x < y })
-    env.setVar(">f", mFunction { x: Float, y: Float -> x > y })
-    env.setVar("<d", mFunction { x: Double, y: Double -> x < y })
-    env.setVar(">d", mFunction { x: Double, y: Double -> x > y })
+    setVar("stdin", `in`)
+    setVar("stdout", out)
+    setVar("stderr", err)
+    setVar("write", mFunction<OutputStream, Char, Unit> { p, c -> p.write(c.toInt()) })
+    setVar("read", mFunction<InputStream, Char> { i -> i.read().toChar() })
 
-    env.setVar("stdin", `in`)
-    env.setVar("stdout", out)
-    env.setVar("stderr", err)
-    env.setVar("write", mFunction<OutputStream, Char, Unit> { p, c -> p.write(c.toInt()) })
-    env.setVar("read", mFunction<InputStream, Char> { i -> i.read().toChar() })
-
-    env.setVar("defmacro", Macro { it: Any ->
+    setVar("defmacro", Macro { it: Any ->
         val name = ((it as ConsList<*>).car as IdentifierExpression).name
         val lambda = it.cdr.car as ConsList<*>
         @Suppress("UNCHECKED_CAST")
-        val macro = Macro(lambda.toIRExpression(env.symbolTable).toEvaluable().eval(env.memory) as MFunction)
-        env.setVar(name, macro)
+        val macro = Macro(lambda.toIRExpression(symbolTable).toEvaluable().eval(memory) as MFunction)
+        setVar(name, macro)
         Nil
     })
 
-    env.setVar("include", Macro { it: Any ->
+    setVar("include", Macro { it: Any ->
         val name = (it as ConsList<*>).car as String
         val file = File(name).absoluteFile
         if (file.isDirectory)
@@ -102,10 +100,8 @@ fun getDefaultRuntimeEnvironment(
                     .let { ConsCell(IdentifierExpression("do"), it.toConsList()) }
     })
 
-    env.setVar("macroexpand", Macro { it: Any -> QuoteExpression((it as SExpression).car.expand(env)) })
+    setVar("macroexpand", Macro { it: Any -> QuoteExpression((it as SExpression).car.expand(this)) })
 
-    env.setVar("print", mFunction<OutputStream, Any, Unit> { p, o -> PrintStream(p).print(o) })
-    env.setVar("println", mFunction<OutputStream, Any, Unit> { p, o -> PrintStream(p).println(o) })
-
-    return env
+    setVar("print", mFunction<OutputStream, Any, Unit> { p, o -> PrintStream(p).print(o) })
+    setVar("println", mFunction<OutputStream, Any, Unit> { p, o -> PrintStream(p).println(o) })
 }
