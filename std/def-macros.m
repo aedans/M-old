e9cd23ef1e67179787c96573b7fe94d9564c4c82
Aedan <aedan.smith@gmail.com>
2017-11-01 01:31:31 -0700

    Fix <= and >=; remove def-macros.m

--- std/def-macros.m
+++
@@ -1,6 +0,0 @@
-(defmacro defm
-  (lambda (x)
-    `(defmacro ,(car x) (lambda ,(car (cdr x)) ~(cdr (cdr x))))))
-
-(defm defn (x)
-  `(def ,(car x) (lambda ,(car (cdr x)) ~(cdr (cdr x)))))
--- std/ops.m
+++ std/ops.m
@@ -1,3 +1,24 @@
+(def >=i (lambda (x y) (! (<i x y))))
+(def >=l (lambda (x y) (! (<l x y))))
+(def >=f (lambda (x y) (! (<f x y))))
+(def >=d (lambda (x y) (! (<d x y))))
+
+(def <=i (lambda (x y) (! (>i x y))))
+(def <=l (lambda (x y) (! (>l x y))))
+(def <=f (lambda (x y) (! (>f x y))))
+(def <=d (lambda (x y) (! (>d x y))))
+
+(def negi (-i 0))
+(def negl (-l 0))
+(def negf (-f 0))
+(def negd (-d 0))
+
+(def !=
+  (lambda (x y)
+    (! (= x y))))
+
+(def nil? (= nil))
+
 (def + +i)
 (def - -i)
 (def * *i)
@@ -5,19 +26,6 @@
 (def % %i)
 (def < <i)
 (def > >i)
-
-(def >=
-  (lambda (x y)
-    (! (< x y))))
-
-(def <=
-  (lambda (x y)
-    (! (> x y))))
-
-(def !=
-  (lambda (x y)
-    (! (= x y))))
-
-(def neg (- 0))
-
-(def nil? (= nil))
+(def >= >=i)
+(def <= <=i)
+(def neg negi)
